77efb23a1ab514b85e246f824ed74e1f17bcca6a
Jake Swogger <jakeswogger1@gmail.com>
2019-10-10 18:58:51 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -2,6 +2,8 @@ def map(source_array)
   return_array = []
   count = 0 
   while count < source_array.length 
-  return_array.push(source_array[count])
-  
+  return_array.push(yield (source_array[count]))
+  count = count + 1 
+end
+return_array
 end
